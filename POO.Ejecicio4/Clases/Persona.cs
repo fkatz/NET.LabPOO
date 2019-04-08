c91f5cf5692d2f6db621cdb592a819f7c10f2c58
frvnnko <49121283+francoGiannassi@users.noreply.github.com>
2019-04-07 22:46:38 -0300

    asdasdasdasdasd

---
+++ POO.Ejecicio4/Clases/Persona.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Persona
+    {
+        private int edad;
+        private string apellido;
+        private int dni;
+        private string nombre;
+
+        public Persona(string nombre, string apellido, int dni)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            DNI = dni;
+        }
+
+        ~Persona()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public int DNI
+        {
+            get
+            {
+                return dni;
+            }
+            set
+            {
+                dni = value;
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public string GetFullName()
+        {
+            return this.Nombre+" "+this.Apellido;
+        }
+
+        public int getAge(DateTime cumpleaños)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - cumpleaños.Year;
+            if (cumpleaños.Month < today.Month && cumpleaños.Day < today.Day) age--;
+            return age;
+        }
+    }
+}
---
+++ POO.Ejecicio4/Ejercicio4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido, fullName;
+            int dni, año, mes, dia;
+
+            Console.WriteLine("Ingrese el nombre de la persona");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el apellido de la persona");
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el dni de la persona");
+            dni = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de año de nacimiento de la persona");
+            año = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de mes de nacimiento de la persona");
+            mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de dia de nacimiento de la persona");
+            dia = int.Parse(Console.ReadLine());
+
+            DateTime fechaNacimiento = new DateTime(año, mes, dia);
+
+            Persona persona = new Persona(nombre, apellido, dni);
+
+            fullName = persona.GetFullName();
+
+            persona.Edad = persona.getAge(fechaNacimiento);
+
+            Console.WriteLine("La edad de " + fullName + " es " + persona.Edad);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
