62751426b93eec1be1126f4dc66130fae6a79f6d
Herno <hernanlistorti@gmail.com>
2019-04-07 23:45:44 -0300

    ej6
    A Nisman lo mataron

---
+++ Lab6/Lab6/Jugada.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lab5
+{
+    public class Jugada
+    {
+        private bool _adivino;
+        private int _intentos;
+        private int _numero;
+
+        public Jugada(int maxNumero)
+        {
+            Random rnd = new Random();
+            Numero = rnd.Next(maxNumero);
+            _intentos = 0;
+        }
+
+        public bool Adivino
+        {
+            get
+            {
+                return this._adivino;
+            }
+            set
+            {
+                this._adivino = value;
+            }
+        }
+
+        public int Intentos
+        {
+            get
+            {
+                return this._intentos;
+            }
+            set
+            {
+                this._intentos = value;
+            }
+        }
+
+        public int Numero
+        {
+            get
+            {
+                return this._numero;
+            }
+            set
+            {
+                this._numero = value;
+            }
+        }
+
+        public virtual void Comparar(int n)
+        {
+            if (n == this._numero)
+            {
+                Adivino = true;
+            }
+            Intentos++;
+        }
+    }
+}
---
+++ Lab6/Lab6/JugadaConAyuda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab5
+{
+    public class JugadaConAyuda : Jugada
+    {
+        public JugadaConAyuda(int maxNumero) : base(maxNumero)
+        {
+        }
+
+        public override void Comparar(int n)
+        {
+            string m = "";
+            if (n == Numero)
+            {
+                Adivino = true;
+            }
+            else
+            {
+                if (n < Numero)
+                {
+                    m += "Tu numero es mayor. ";
+                }
+                else
+                {
+                    m += "Tu numero es menor. ";
+                }
+                if (Math.Abs(n - Numero) >= 100)
+                {
+                    m += "Estás muy lejos.";
+                }
+                else if (Math.Abs(n - Numero) <= 5)
+                {
+                    m += "Estás muy cerca.";
+                }
+                Console.WriteLine(m);
+            }
+            Intentos++;
+        }
+    }
+}
